0dfe4a62fbfa90b3d05a6b0f8c4f2372fc375d2d
Danielle Bradshaw <bradshawdanni@gmail.com>
2026-06-30 19:11:24 +1000

    Add SATCOM package initializer

---
+++ src/mh370_inverse_inference/satcom/__init__.py
@@ -0,0 +1,5 @@
+"""SATCOM geometry and measurement models."""
+
+from mh370_inverse_inference.satcom.bto import SPEED_OF_LIGHT_M_S, timing_error_to_range_m
+
+__all__ = ["SPEED_OF_LIGHT_M_S", "timing_error_to_range_m"]
