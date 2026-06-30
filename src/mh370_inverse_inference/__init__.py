71f4ab5450342959f97387dcb82596c7e0c1b9ad
Danielle Bradshaw <bradshawdanni@gmail.com>
2026-06-30 19:11:18 +1000

    Add package initializer

---
+++ src/mh370_inverse_inference/__init__.py
@@ -0,0 +1,6 @@
+"""MH370 inverse-inference research package."""
+
+from mh370_inverse_inference.satcom.bto import timing_error_to_range_m
+
+__all__ = ["timing_error_to_range_m"]
+__version__ = "0.1.0"
