863eb24dc792a1250fccd09fa34e291c81923995
Danielle Bradshaw <bradshawdanni@gmail.com>
2026-06-30 19:18:49 +1000

    Fix SATCOM import formatting

--- src/mh370_inverse_inference/satcom/__init__.py
+++ src/mh370_inverse_inference/satcom/__init__.py
@@ -1,5 +1,8 @@
 """SATCOM geometry and measurement models."""
 
-from mh370_inverse_inference.satcom.bto import SPEED_OF_LIGHT_M_S, timing_error_to_range_m
+from mh370_inverse_inference.satcom.bto import (
+    SPEED_OF_LIGHT_M_S,
+    timing_error_to_range_m,
+)
 
 __all__ = ["SPEED_OF_LIGHT_M_S", "timing_error_to_range_m"]
