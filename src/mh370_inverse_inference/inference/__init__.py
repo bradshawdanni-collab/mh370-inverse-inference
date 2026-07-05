6b5b94ec4083a516c0cabd480be143600c024971
Danielle Bradshaw <bradshawdanni@gmail.com>
2026-07-05 21:16:07 +1000

    Export posterior normalization interfaces

--- src/mh370_inverse_inference/inference/__init__.py
+++ src/mh370_inverse_inference/inference/__init__.py
@@ -1,1 +1,13 @@
 """Probabilistic inference interfaces built above deterministic L1-L5 outputs."""
+
+from mh370_inverse_inference.inference.posterior import (
+    HypothesisScore,
+    PosteriorProbability,
+    normalize_posteriors,
+)
+
+__all__ = [
+    "HypothesisScore",
+    "PosteriorProbability",
+    "normalize_posteriors",
+]
