0456a50398e9bc37a2bdaec638f273868ebc774b
Danielle Bradshaw <bradshawdanni@gmail.com>
2026-07-12 12:20:23 +1000

    Add L3.1 interpretation package

---
+++ src/mh370_inverse_inference/interpretation/__init__.py
@@ -0,0 +1,8 @@
+"""Deterministic L3.1 interpretation input boundary."""
+
+from mh370_inverse_inference.interpretation.models import InterpretationRequest
+from mh370_inverse_inference.interpretation.projection import (
+    build_interpretation_request,
+)
+
+__all__ = ["InterpretationRequest", "build_interpretation_request"]
