50dc2a10db9918fce3e7f99be510109a4cd5ff95
Danielle Bradshaw <bradshawdanni@gmail.com>
2026-07-12 16:38:41 +1000

    Export L5.1 hypothesis definition contract

--- src/mh370_inverse_inference/hypothesis/__init__.py
+++ src/mh370_inverse_inference/hypothesis/__init__.py
@@ -1,11 +1,19 @@
 """Deterministic hypothesis evaluation contracts."""
 
+from mh370_inverse_inference.hypothesis.definition import (
+    HypothesisDefinition,
+    HypothesisType,
+    build_hypothesis_definition,
+)
 from mh370_inverse_inference.hypothesis.request import (
     HypothesisEvaluationRequest,
     build_hypothesis_evaluation_request,
 )
 
 __all__ = [
+    "HypothesisDefinition",
     "HypothesisEvaluationRequest",
+    "HypothesisType",
+    "build_hypothesis_definition",
     "build_hypothesis_evaluation_request",
 ]
