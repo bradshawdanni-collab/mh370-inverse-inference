1da456fde5a48ab350a8482d63d7da8fb595313a
Danielle Bradshaw <bradshawdanni@gmail.com>
2026-08-01 12:14:50 +1000

    Export isolated L1.1 state input API

--- src/mh370_inverse_inference/aircraft/__init__.py
+++ src/mh370_inverse_inference/aircraft/__init__.py
@@ -1,13 +1,13 @@
 """Aircraft performance and reachability models."""
 
-from mh370_inverse_inference.aircraft.state import (
+from mh370_inverse_inference.aircraft.state_contract import (
     AIRCRAFT_STATE_CONTRACT_VERSION,
-    AircraftState,
+    AircraftStateInput,
     AircraftStateTransition,
 )
 
 __all__ = (
     "AIRCRAFT_STATE_CONTRACT_VERSION",
-    "AircraftState",
+    "AircraftStateInput",
     "AircraftStateTransition",
 )
