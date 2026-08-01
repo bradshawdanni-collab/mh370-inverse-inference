a8a122fc9f862557df617756b31ae50fb209c172
Danielle Bradshaw <bradshawdanni@gmail.com>
2026-08-01 12:50:38 +1000

    Fix Ruff and Black formatting for envelope contract

--- src/mh370_inverse_inference/aircraft/envelope_contract.py
+++ src/mh370_inverse_inference/aircraft/envelope_contract.py
@@ -45,20 +45,33 @@ class AircraftOperatingEnvelope:
     contract_version: str = AIRCRAFT_ENVELOPE_CONTRACT_VERSION
 
     def __post_init__(self) -> None:
-        minimum_speed = _finite_non_negative(self.minimum_speed_mps, "minimum_speed_mps")
-        maximum_speed = _finite_non_negative(self.maximum_speed_mps, "maximum_speed_mps")
+        minimum_speed = _finite_non_negative(
+            self.minimum_speed_mps,
+            "minimum_speed_mps",
+        )
+        maximum_speed = _finite_non_negative(
+            self.maximum_speed_mps,
+            "maximum_speed_mps",
+        )
         minimum_altitude = _finite_non_negative(
-            self.minimum_altitude_m, "minimum_altitude_m"
+            self.minimum_altitude_m,
+            "minimum_altitude_m",
         )
         maximum_altitude = _finite_non_negative(
-            self.maximum_altitude_m, "maximum_altitude_m"
-        )
-        _finite_non_negative(self.maximum_climb_rate_mps, "maximum_climb_rate_mps")
-        _finite_non_negative(
-            self.maximum_descent_rate_mps, "maximum_descent_rate_mps"
+            self.maximum_altitude_m,
+            "maximum_altitude_m",
         )
         _finite_non_negative(
-            self.maximum_turn_rate_deg_s, "maximum_turn_rate_deg_s"
+            self.maximum_climb_rate_mps,
+            "maximum_climb_rate_mps",
+        )
+        _finite_non_negative(
+            self.maximum_descent_rate_mps,
+            "maximum_descent_rate_mps",
+        )
+        _finite_non_negative(
+            self.maximum_turn_rate_deg_s,
+            "maximum_turn_rate_deg_s",
         )
 
         if minimum_speed > maximum_speed:
